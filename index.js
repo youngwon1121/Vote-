b122e333f4caa76bbf37fc8034a90333dc2bf70c
youngwon <youngwon1121@naver.com>
2022-06-12 15:35:16 +0900

    UI수정

--- index.js
+++ index.js
@@ -37,18 +37,33 @@ async function get_info(address) {
     // console.log(web3.eth.Contract)
     let contract = new web3.eth.Contract(abi,address);
     console.log(contract.methods)
-    k0 = await contract.methods.candidatorList(0).call()
-    k1 = await contract.methods.candidatorList(1).call()
-    // k2 = await contract.methods.myFunction().call({from:account})
-    // console.log(k2)
-    k = [k0,k1]
+
+    var candidatorLength = await contract.methods.getCandidatorCount().call({from:account})
+    console.log(candidatorLength)
+
+    k = []
+    for (var i=0; i<candidatorLength; i++) {
+        k.push(await contract.methods.candidatorList(i).call())
+    }
+
     let data = {
         'title' : await contract.methods.title().call(),
         'candidators' : k,
         'startDateTime' : await contract.methods.startDateTime().call(),
         'endDateTime' : await contract.methods.endDateTime().call(),
+        'live' : await contract.methods.live().call(),
+        'owner': await contract.methods.owner().call()
     }
     return data
+}
+
+async function finishVote(address) {
+    let abi  = await $.getJSON('./abi.json');
+    // console.log(web3.eth.Contract)
+    let contract = new web3.eth.Contract(abi,address);
+    await contract.methods.finishVote().send({
+        from: account
+    })
 }
 
 async function deploy(constructor_args) {
@@ -67,11 +82,6 @@ async function deploy(constructor_args) {
         console.log(error);
     });
 
-    // console.log(constructor_args['title']);
-    // console.log(contractInstance.address);
-    // console.log(account);
-
-
     insert_contract(constructor_args[0], contractInstance.options.address, account)
 }
 
@@ -84,43 +94,8 @@ async function upVote(address, data) {
     })
 }
 
-
-
-
-// abi = JSON.parse('[{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
-
-// contract = new web3.eth.Contract(abi);
-// contract.options.address = "0x14BaeB20C6E5000AF1B5874cDe44Cb6E8A42fb90";
-// // update this contract address with your contract address
-
-// candidates = {"Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3"}
-
-// function voteForCandidate(candidate) {
-//  candidateName = $("#candidate").val();
-//  console.log(candidateName);
-
-//  contract.methods.voteForCandidate(web3.utils.asciiToHex(candidateName)).send({from: account}).then((f) => {
-//   let div_id = candidates[candidateName];
-//   contract.methods.totalVotesFor(web3.utils.asciiToHex(candidateName)).call().then((f) => {
-//    $("#" + div_id).html(f);
-//   })
-//  })
-// }
-
-// $(document).ready(function() {
-//  candidateNames = Object.keys(candidates);
-
-//  for(var i=0; i<candidateNames.length; i++) {
-//  let name = candidateNames[i];
-  
-//  contract.methods.totalVotesFor(web3.utils.asciiToHex(name)).call().then((f) => {
-//   $("#" + candidates[name]).html(f);
-//  })
-//  }
-// });
-
-
 function dateFormat(date) {
+    date.setUTCHours(-9);
     let month = date.getMonth() + 1;
     let day = date.getDate();
     let hour = date.getHours();
@@ -133,5 +108,6 @@ function dateFormat(date) {
     minute = minute >= 10 ? minute : '0' + minute;
     second = second >= 10 ? second : '0' + second;
 
-    return date.getFullYear() + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
+    // return date.getFullYear() + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
+    return date.getFullYear() + '-' + month + '-' + day;
 }
